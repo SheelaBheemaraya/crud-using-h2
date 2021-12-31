671121f0c68774aac6d1a192126985f2db2a0f82
SheelaBheemaraya <sb3891604@gmail.com>
2021-12-31 18:16:15 +0530

    crud using h2 in memory database

---
+++ crud-using-db/src/main/java/com/rest/db/crudusingdb/CrudUsingDbApplication.java
@@ -0,0 +1,13 @@
+package com.rest.db.crudusingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsingDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsingDbApplication.class, args);
+	}
+
+}
---
+++ crud-using-db/src/test/java/com/rest/db/crudusingdb/CrudUsingDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.db.crudusingdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsingDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
